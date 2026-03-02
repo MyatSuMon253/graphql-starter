1d1d0e4ca6bb1acdd53dcee62c4ac0bad2592a6e
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-03-02 22:34:14 +0630

    chore: codegen for client

---
+++ apolloclient/src/__generated__/graphql.ts
@@ -0,0 +1,77 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = T | null | undefined;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Author = {
+  __typename?: 'Author';
+  email: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  name?: Maybe<Scalars['String']['output']>;
+  post?: Maybe<Array<Maybe<Post>>>;
+};
+
+export type CreatePostResponse = {
+  __typename?: 'CreatePostResponse';
+  code: Scalars['Int']['output'];
+  message: Scalars['String']['output'];
+  post?: Maybe<Post>;
+  success: Scalars['Boolean']['output'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createPost: CreatePostResponse;
+  deletePost: Scalars['Boolean']['output'];
+};
+
+
+export type MutationCreatePostArgs = {
+  input?: InputMaybe<PostInput>;
+};
+
+
+export type MutationDeletePostArgs = {
+  id: Scalars['ID']['input'];
+};
+
+export type Post = {
+  __typename?: 'Post';
+  author?: Maybe<Author>;
+  content?: Maybe<Scalars['String']['output']>;
+  id: Scalars['ID']['output'];
+  published: Scalars['Boolean']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type PostInput = {
+  authorId: Scalars['ID']['input'];
+  content?: InputMaybe<Scalars['String']['input']>;
+  published: Scalars['Boolean']['input'];
+  title: Scalars['String']['input'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  /** Get a post by id */
+  post: Post;
+  /** Get all posts */
+  posts: Array<Post>;
+};
+
+
+export type QueryPostArgs = {
+  id: Scalars['ID']['input'];
+};
